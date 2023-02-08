6956503d3461db697d21c1bca82a5959c7ab75f7
Stas Spiridonov <limserv17@gmail.com>
2023-02-08 17:35:30 +0200

    components lits-item

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,13 +1,16 @@
 import AppInfo from "../app-info/app-info";
 import SearchPanel from "../search-panal/search-panel";
 import "./app.css";
-
+import AppFilter from "../app-filter/app-filter";
+import EmployeesListItem from "../employees-list-item/employees-list-item";
 function App() {
   return (
     <div className="app">
       <AppInfo />
       <div className="search-panel">
         <SearchPanel />
+        <AppFilter />
+        <EmployeesListItem />
       </div>
     </div>
   );
